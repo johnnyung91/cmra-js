68338517e8a51e4b5f8a11770d0877f18cea28e8
Johnny Ung <johnnyung91@gmail.com>
2020-04-24 03:42:47 +0000

    fixed some styling with price font color and spacing control

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 function ProductListItem(props) {
   const { product } = props;
-  const price = (product.price / 100).toFixed(2);
+  const currency = (product.price / 100).toFixed(2);
   return (
     <div className="col-md-4 mb-4">
       <div className="card mb-4 shadow-sm product">
         <img
-          className="card-img-top"
+          className="card-img-top py-3"
           src={product.image}
           alt={product.name}
         />
-        <div className="card-body">
+        <div className="card-body py-0">
           <h5 className="card-title">{product.name}</h5>
-          <p className="card-text">${price}</p>
+          <p className="card-text text-muted">${currency}</p>
           <p className="card-text">{product.shortDescription}</p>
         </div>
       </div>
