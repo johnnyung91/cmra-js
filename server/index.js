acf7dab51a7c153226a5b72755436291a15cf0ea
Johnny Ung <johnnyung91@gmail.com>
2020-04-24 06:58:29 +0000

    started GET endpoint, unsure what next() clientError refers to; successful test for unfound ID otherwise written

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
-});
+// delete?
+// app.get('/api/health-check', (req, res, next) => {
+//   db.query('select \'successfully connected\' as "message"')
+//     .then(result => res.json(result.rows[0]))
+//     .catch(err => next(err));
+// });
 
 // view products back end
 app.get('/api/products', (req, res, next) => {
@@ -36,6 +37,16 @@ app.get('/api/products', (req, res, next) => {
       res.status(200).json(products);
     })
     .catch(err => next(err));
+});
+
+app.get('/api/products/:productId', (req, res, next) => {
+  const { productId } = req.params;
+  if (!parseInt(productId, 10)) {
+    return res.status(400).json({
+      error: '"productId" must be a positive integer'
+    });
+  }
+
 });
 
 app.use('/api', (req, res, next) => {
