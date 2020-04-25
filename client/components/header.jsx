19dd14cec421920177ab21941324c8618f97cab2
Johnny Ung <johnnyung91@gmail.com>
2020-04-25 22:41:21 +0000

    cleaned up Header component to involve less classes; able to render what cart should look like; react props inc

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,11 +4,17 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="navbar-dark bg-dark shadow py-2">
-        <div className="container d-flex justify-content-between">
-          <div className="navbar-brand d-flex align-items-center align-middle">
-            <h5 className="m-0">
+        <div className="container d-flex justify-content-between py-2">
+          <div className="title-heading text-light">
+            <h5>
               <i className="fas fa-dollar-sign mr-2"></i>Wicked Sales
             </h5>
+          </div>
+          <div className="cart-heading text-light">
+            <h6>
+              0 Items
+              <i className="fas fa-shopping-cart ml-2"></i>
+            </h6>
           </div>
         </div>
       </div>
