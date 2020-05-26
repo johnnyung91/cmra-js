ad2cbf12456e31b62225b7483bb193453b6945a3
Johnny Ung <johnnyung91@gmail.com>
2020-05-26 21:41:02 +0000

    added fade in and slide in animations to modal

--- client/components/enter-modal.jsx
+++ client/components/enter-modal.jsx
@@ -4,17 +4,17 @@ export default class EnterModal extends React.Component {
 
   render() {
     return (
-      <div className="enter-modal">
+      <div className="enter-modal fade-in">
         <div className="modal-overlay"></div>
-        <div className="modal-dialog w-75">
+        <div className="modal-dialog w-75 slide-in">
           <div className="modal-content">
-            <div className="modal-header flex-wrap justify-content-center pb-0">
+            <div className="modal-header flex-wrap justify-content-center modal-text pb-0">
               <h5 className="modal-title">Welcome to Wicked Sales!</h5>
-              <p>Wicked Sales is a full-stack E-commerce website built using React.js and is to be used strictly for demonstrative purposes</p>
+              <p>Wicked Sales is a full-stack E-commerce website built using React.js and is to be used <strong>strictly for demonstrative purposes</strong></p>
             </div>
-            <div className="modal-body text-align-center">
+            <div className="modal-body modal-text">
               <p>
-              By clicking the button below, I accept that no real purchases will be made, no payment processing will be done, and that personal information such as real names, addresses, and credit card numbers should not be used.
+              By clicking the button below, I accept that <strong>no real purchases will be made, no payment processing will be done, and personal information such as real names, addresses, and credit card numbers should not be used</strong>.
               </p>
               <button type="button" className="btn btn-danger btn-block close-modal">Close</button>
             </div>
