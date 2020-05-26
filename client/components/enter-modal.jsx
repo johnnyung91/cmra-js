780cea2c28ee0a2a1c53186cd665bf7f352a9bbe
Johnny Ung <johnnyung91@gmail.com>
2020-05-26 21:55:55 +0000

    added handleClick method to animate modal

--- client/components/enter-modal.jsx
+++ client/components/enter-modal.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 export default class EnterModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const enterModal = document.getElementById('enter-modal');
+    const modalDialog = document.getElementById('enter-dialog');
+    enterModal.classList.remove('fade-in');
+    enterModal.classList.add('fade-out');
+    modalDialog.classList.remove('slide-in');
+    modalDialog.classList.add('slide-out');
+  }
 
   render() {
     return (
-      <div className="enter-modal fade-in">
+      <div className="enter-modal fade-in" id="enter-modal">
         <div className="modal-overlay"></div>
-        <div className="modal-dialog w-75 slide-in">
+        <div className="modal-dialog w-75 slide-in" id="enter-dialog">
           <div className="modal-content">
             <div className="modal-header flex-wrap justify-content-center modal-text pb-0">
               <h5 className="modal-title">Welcome to Wicked Sales!</h5>
@@ -16,7 +29,10 @@ export default class EnterModal extends React.Component {
               <p>
               By clicking the button below, I accept that <strong>no real purchases will be made, no payment processing will be done, and personal information such as real names, addresses, and credit card numbers should not be used</strong>.
               </p>
-              <button type="button" className="btn btn-danger btn-block close-modal">Close</button>
+              <button
+                type="button"
+                className="btn btn-danger btn-block close-modal"
+                onClick={this.handleClick}>Close</button>
             </div>
           </div>
         </div>
