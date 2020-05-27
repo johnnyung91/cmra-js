7e2944ecb679bc4d418c0e8e729d416e50ce95a0
Johnny Ung <johnnyung91@gmail.com>
2020-05-27 07:51:21 +0000

    added HTML skeleton to modal

--- client/components/add-item-modal.jsx
+++ client/components/add-item-modal.jsx
@@ -4,7 +4,21 @@ export default class AddItemModal extends React.Component {
 
   render() {
     return (
-      <h1>hello world</h1>
+      <div className="enter-modal fade-in" id="enter-modal">
+        <div className="modal-overlay"></div>
+        <div className="modal-dialog w-75 slide-in" id="enter-dialog">
+          <div className="modal-content">
+            <div className="modal-header flex-wrap justify-content-center modal-text pb-0">
+              <h5 className="modal-title">Item Added To Cart!</h5>
+              <p>Item has been added to the cart</p>
+            </div>
+            <div className="modal-body modal-text">
+              <button type="button" className="btn btn-danger close-modal">Continue Shopping</button>
+              <button type="button" className="btn btn-danger close-modal">View Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
