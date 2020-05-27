bea1b36a31c7441b4f9fa67f91ee2258d44948d2
Johnny Ung <johnnyung91@gmail.com>
2020-05-27 08:20:33 +0000

    added styling to buttons and font color

--- client/components/add-item-modal.jsx
+++ client/components/add-item-modal.jsx
@@ -9,16 +9,18 @@ export default class AddItemModal extends React.Component {
           <div className="modal-content">
             <div className="modal-header flex-wrap modal-text pb-0">
               <h4 className="modal-title w-100 pb-2">Item Added To Cart!</h4>
-              <p className="w-100">{this.props.product.name} has been added to the cart</p>
+              <p className="w-100">
+                <span className="text-muted">{this.props.product.name}</span> has been added to the cart.
+              </p>
             </div>
             <div className="modal-body d-flex">
               <div className="col-6">
-                <button type="button" className="btn btn-danger btn-block close-modal">
+                <button type="button" className="btn btn-secondary btn-block close-modal">
                   Continue Shopping
                 </button>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-danger btn-block close-modal">
+                <button type="button" className="btn btn-primary btn-block close-modal">
                   View Cart
                 </button>
               </div>
