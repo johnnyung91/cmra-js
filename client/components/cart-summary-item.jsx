55d1b5451f84ce0e09deb91d71b3c29a53791e4f
Johnny Ung <johnnyung91@gmail.com>
2020-05-28 20:38:34 +0000

    added remove item button to cart item; applied styling for all formats

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -7,16 +7,23 @@ function CartSummaryItem(props) {
     <div className="py-3">
       <div className="container px-4 border rounded-lg shadow">
         <div className="row">
-          <div className="col-lg-4 p-2 text-center">
+          <div className="col-md-4 col-lg-4 p-2 text-center">
             <img
               src={cartItem.image}
               alt={cartItem.name}
               className="fit p-2"/>
           </div>
-          <div className="col-lg-8 d-flex flex-column justify-content-center summary">
-            <h3>{cartItem.name}</h3>
-            <p className="text-muted">${currency}</p>
-            <p>{cartItem.shortDescription}</p>
+          <div className="col-md-8 col-lg-8 d-flex align-items-center summary py-4">
+            <div>
+              <h3 className="cart-item-heading mb-3">{cartItem.name}</h3>
+              <p className="text-muted">${currency}</p>
+              <p>{cartItem.shortDescription}</p>
+              <div className="text-muted remove-item pointer">
+                <span>
+                  <i className="fas fa-times red"/> Remove Item From Cart
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
