387063c7905ef0d6e0577f048f8b750e0645b155
Johnny Ung <johnnyung91@gmail.com>
2020-05-29 05:10:11 +0000

    added animation to modal when cancelling remove option

--- client/components/remove-item-modal.jsx
+++ client/components/remove-item-modal.jsx
@@ -4,6 +4,7 @@ export default class RemoveModal extends React.Component {
   constructor(props) {
     super(props);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
   handleRemove() {
@@ -12,21 +13,30 @@ export default class RemoveModal extends React.Component {
     resetState();
   }
 
+  handleCancel() {
+    const { resetState } = this.props;
+    const removeModal = document.getElementById('remove-modal');
+    const removeDialog = document.getElementById('remove-dialog');
+    removeModal.className = 'remove-modal fade-out';
+    removeDialog.className = 'modal-dialog w-75 slide-out';
+    setTimeout(() => resetState(), 450);
+  }
+
   render() {
-    const { product, resetState } = this.props;
+    const { product } = this.props;
 
     return (
       <div className="remove-modal fade-in" id="remove-modal">
         <div className="modal-overlay"></div>
         <div className="modal-dialog w-75 slide-in" id="remove-dialog">
           <div className="modal-content">
-            <div className="close-button pointer" onClick={resetState}>
+            <div className="close-button pointer" onClick={() => this.handleCancel()}>
               <i className="far fa-times-circle text-secondary"></i>
             </div>
             <div className="modal-header flex-wrap modal-text pb-0">
               <h4 className="modal-title w-100 pb-2">{product.name}</h4>
               <p className="w-100">
-                sample header
+                Are you sure you want to remove this item?
               </p>
             </div>
             <div className="modal-body d-flex flex-wrap add-body">
@@ -34,7 +44,7 @@ export default class RemoveModal extends React.Component {
                 <button
                   type="button"
                   className="btn btn-secondary btn-block close-modal"
-                  onClick={resetState}>
+                  onClick={() => this.handleCancel()}>
                   Cancel
                 </button>
               </div>
