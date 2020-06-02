0248d704e4f6c659697bf93a1e624c414383d5d9
Johnny Ung <johnnyung91@gmail.com>
2020-06-02 06:45:41 +0000

    added new fonts style sheet for custom fonts

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -15,7 +15,7 @@ export default class Header extends React.Component {
           <div className="cart-heading text-light pointer" onClick={() => setView('cart', {})}>
             <h6>
               {cartItemCount.length} {cartItemCount.length === 1 ? 'Item' : 'Items'}
-              <i className="fas fa-shopping-cart ml-2"></i>
+              <i className="fas fa-shopping-cart cart ml-2"></i>
             </h6>
           </div>
         </div>
