c2a04e2d51aab67b9c88e2b155e6926ce7ffe35f
Johnny Ung <johnnyung91@gmail.com>
2020-06-02 10:08:55 +0000

    added sticky footer; needs styling changes for linear transition

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 export default function Footer(props) {
   return (
-    <div className="navbar-dark bg-dark shadow">
-      <div className="container d-flex justify-content-center align-items-center py-2">
-        <div >
-          <div className="footer-center">
-            <img src="/images/cmra-logo.png" alt="CMRA" className='footer-logo'/>
-            <div className="footer-text pb-4">
-              <h3>C M R A</h3>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, nihil?</p>
+    <footer>
+      <div className="navbar-dark bg-dark shadow">
+        <div className="container d-flex justify-content-center align-items-center py-2">
+          <div >
+            <div className="footer-center">
+              <img src="/images/cmra-logo.png" alt="CMRA" className='footer-logo'/>
+              <div className="footer-text pb-4">
+                <h3>C M R A</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, nihil?</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
